0c94ffc9e26d55a3f3b2e57e1fdedebac945faca
azawy-iq <anwar.nille@gmail.com>
2026-08-06 00:13:48 +0300

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -1,76 +1,47 @@
 import UIKit
-import WebKit
-import AVFoundation
 
-class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
-    
-    var webView: WKWebView!
+class ViewController: UIViewController {
+
+    private let rotatingImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "AppIcon") // استخدام صورة AppIcon الموجودة في المستودع
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
         
-        // 1. تهيئة جلسة الصوت لتسمح بالتشغيل في الخلفية وبث الفيديو
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .moviePlayback, options: [])
-            try AVAudioSession.sharedInstance().setActive(true)
-        } catch {
-            print("خطأ في ضبط جلسة الصوت: \(error)")
-        }
-
-        // 2. إعدادات WKWebView لتفعيل Picture-in-Picture
-        let config = WKWebViewConfiguration()
-        config.allowsInlineMediaPlayback = true
-        config.allowsPictureInPictureMediaPlayback = true
-        config.mediaTypesRequiringUserActionForPlayback = []
+        view.addSubview(rotatingImageView)
         
-        config.preferences.javaScriptCanOpenWindowsAutomatically = false
+        NSLayoutConstraint.activate([
+            rotatingImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            rotatingImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            rotatingImageView.widthAnchor.constraint(equalToConstant: 150),
+            rotatingImageView.heightAnchor.constraint(equalToConstant: 150)
+        ])
         
-        // حظر الإعلانات بالنوافذ المنبثقة
-        let blockScript = "window.open = function() { return null; };"
-        let userScript = WKUserScript(source: blockScript, injectionTime: .atDocumentStart, forMainFrameOnly: false)
-        config.userContentController.addUserScript(userScript)
-
-        // 3. إنشاء الـ WebView
-        webView = WKWebView(frame: self.view.bounds, configuration: config)
-        webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        webView.navigationDelegate = self
-        webView.uiDelegate = self
-        
-        webView.backgroundColor = .black
-        webView.scrollView.backgroundColor = .black
-        
-        self.view.addSubview(webView)
-        
-        if let url = URL(string: "https://starcima.com") {
-            let request = URLRequest(url: url)
-            webView.load(request)
-        }
+        startRotating()
     }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        return .all
-    }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        if let url = navigationAction.request.url {
-            let host = url.host?.lowercased() ?? ""
-            if host.contains("starcima") || navigationAction.navigationType == .other {
-                decisionHandler(.allow)
-                return
-            }
-            if navigationAction.navigationType == .linkActivated || navigationAction.targetFrame == nil {
-                decisionHandler(.cancel)
-                return
+    private func rotatingAnimation() {
+        UIView.animate(withDuration: 4.0, delay: 0.0, options: [.curveLinear], animations: {
+            self.rotatingImageView.transform = self.rotatingImageView.transform.rotated(by: CGFloat.pi)
+        }) { finished in
+            if finished {
+                self.rotatingAnimation()
             }
         }
-        decisionHandler(.allow)
     }
 
-    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
-        return nil
+    private func startRotating() {
+        // حلقة تدوير مستمرة للأيقونة داخل التطبيق
+        Timer.scheduledTimer(withTimeInterval: 4.0, repeats: true) { _ in
+            UIView.animate(withDuration: 4.0, delay: 0, options: .curveLinear) {
+                self.rotatingImageView.transform = self.rotatingImageView.transform.rotated(by: .pi * 2)
+            }
+        }
     }
 }
